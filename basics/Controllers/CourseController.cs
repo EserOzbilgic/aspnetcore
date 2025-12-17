cb875326d523e61f0ac4b92f4493d80791bd4c8b
Eser Özbilgiç <you@example.com>
2025-12-17 10:50:09 +0300

    Setting up files and creating the project

---
+++ basics/Controllers/CourseController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+namespace basics.Controllers;
+
+public class CourseController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+      public string List()
+    {
+        return "Course/List";
+    }
+
+
+
+}
---
+++ basics/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using basics.Models;
+
+namespace basics.Controllers;
+
+public class HomeController : Controller
+{
+    public string Index()
+    {
+        return "Home/Index";
+    }
+
+    public string Contact()
+    {
+        return "Home/Contact";
+    }
+
+}
